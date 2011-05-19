d222dc4dc27122e3dea8a4e37ccec0a328bf34e6
Sam Lee-Lindsay <sleelin@gmail.com>
2011-05-20 04:16:35 +1000

    initial commit

--- src/com/sleelin/pvptoggle/PvPToggleEntityListener.java
+++ src/com/sleelin/pvptoggle/PvPToggleEntityListener.java
@@ -1,11 +1,46 @@
 package com.sleelin.pvptoggle;
 
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityListener;
 
 public class PvPToggleEntityListener extends EntityListener {
-
-	public PvPToggleEntityListener(PvPToggle pvPToggle) {
-		// TODO Auto-generated constructor stub
+	
+	public static PvPToggle plugin;
+	private EntityDamageByEntityEvent edentitye;
+	
+	public PvPToggleEntityListener(PvPToggle instance) {
+		plugin = instance;
 	}
+	
+	public void onEntityDamage(EntityDamageEvent event){
+		if (event.getEntity() instanceof Player){
+			Player player = (Player) event.getEntity();
+			boolean haspermissions = false;
+
+			if (PvPToggle.permissionHandler != null){
+				if (PvPToggle.permissionHandler.has(player, "pvptoggle")){
+					haspermissions = true;
+				}
+			} else if (player.isOp()){
+				haspermissions = true;
+			}
+			
+			if (haspermissions){
+				boolean isenabled = plugin.pvpEnabled(player);
+				if (!isenabled){
+					Player defender = (Player) edentitye.getDamager();
+					String message = ChatColor.RED + player.getDisplayName() + "has PvP disabled!";
+					defender.sendMessage(message);
+					event.setCancelled(true);
+				}
+				
+			}
+		}
+	}
+	
+	
 
 }
--- src/com/sleelin/pvptoggle/PvPTogglePlayerListener.java
+++ src/com/sleelin/pvptoggle/PvPTogglePlayerListener.java
@@ -1,11 +1,40 @@
 package com.sleelin.pvptoggle;
 
 import org.bukkit.event.player.PlayerListener;
+import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.entity.Player;
+
+
 
 public class PvPTogglePlayerListener extends PlayerListener {
 
-	public PvPTogglePlayerListener(PvPToggle pvPToggle) {
-		// TODO Auto-generated constructor stub
+	public static PvPToggle plugin;
+	
+	public PvPTogglePlayerListener(PvPToggle instance) {
+		plugin = instance;
+	}
+	
+	public void onPlayerCommand(PlayerChatEvent event){
+		String[] command = event.getMessage().split(" ");
+		Player player = event.getPlayer();
+		boolean haspermissions = false;
+
+		if (PvPToggle.permissionHandler != null){
+			if (PvPToggle.permissionHandler.has(player, "pvptoggle")){
+				haspermissions = true;
+			}
+		} else if (player.isOp()){
+			haspermissions = true;
+		}
+		
+		if (haspermissions){
+			if ((command[0].equalsIgnoreCase("/pvp"))&&((command[1].equalsIgnoreCase("on"))||(command[1].equalsIgnoreCase("enable")))){
+				plugin.pvpEnable(player);
+			} else if ((command[0].equalsIgnoreCase("/pvp"))&&((command[1].equalsIgnoreCase("off"))||(command[1].equalsIgnoreCase("disable")))){
+				plugin.pvpDisable(player);
+			}
+			event.setCancelled(true);
+		}
 	}
 
 }
